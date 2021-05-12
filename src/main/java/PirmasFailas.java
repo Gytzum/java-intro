6b1ce657ee98fd97ce05fffc8f5cd875ae67e586
Gytzum <gytzum@gmail.com>
2021-05-12 13:21:07 +0300

    uzduotis-nr1

--- src/main/java/PirmasFailas.java
+++ src/main/java/PirmasFailas.java
@@ -1,7 +1,14 @@
 public class PirmasFailas {
-
     public static void main(String[] args) {
-        System.out.println("jhvhj");
-
+        int a = 5;
+        int b = 6;
+        int c = 5;
+        if (a==b && b==c) {
+            System.out.println("Lygiakrastis");
+        }else if (a==b ||a==c){
+            System.out.println("Lygiasonis");
+        }else {
+            System.out.println("Ivairiakrastis");
+        }
     }
 }
